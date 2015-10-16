87406d7c20ec0fda9db6db3998138cd91393e3c7
PEMapModder <pemapmodder1970@gmail.com>
2015-10-16 18:40:38 +0800

    Added Katana build

---
+++ katana.php
@@ -0,0 +1,25 @@
+<?php
+
+chdir(__DIR__);
+
+if(is_file("build/Katana.phar")){
+	unlink("build/Katana.phar");
+}
+
+$phar = new Phar("build/Katana.phar");
+$phar->setSignatureAlgorithm(Phar::SHA1);
+$phar->setStub('<?php define("pocketmine\\\\PATH", "phar://". __FILE__ ."/"); require_once("phar://". __FILE__ ."/src/pocketmine/PocketMine.php");  __HALT_COMPILER();');
+$phar->startBuffering();
+$filePath = "./";
+foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($filePath . "src")) as $file){
+	$path = ltrim(str_replace(["\\", $filePath], ["/", ""], $file), "/");
+	if($path{0} === "." or strpos($path, "/.") !== false or substr($path, 0, 4) !== "src/"){
+		continue;
+	}
+	$phar->addFile($file, $path);
+	echo "\rAdding $path";
+}
+echo "\n";
+$phar->stopBuffering();
+
+exec("git add -A");
